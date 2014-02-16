abe5660042f6b1e6d99d9a23eb85f9034136f53f
Ricardo Costa <ricardo@ricardo-macbook.home>
2014-02-16 23:41:39 +0000

    added Context class and shallow dependencies

--- sparrow-sharp/Sparrow/Core/Texture.cs
+++ sparrow-sharp/Sparrow/Core/Texture.cs
@@ -4,6 +4,10 @@ namespace Sparrow.Core
 {
 	public class Texture
 	{
+		public string Name { get; set; }
+		public uint NativeWidth { get; set; }
+		public uint NativeHeight { get; set; }
+
 		public Texture ()
 		{
 		}
---
+++ sparrow-sharp/Sparrow/SP.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Sparrow.Core;
+
+namespace Sparrow
+{
+	public static class SP
+	{
+		public static ViewController CurrentController { get; set; }
+	}
+}
+
