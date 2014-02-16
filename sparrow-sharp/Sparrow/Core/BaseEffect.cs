b6e20e719db7f79e0e6f4c227f4f6259314fbee7
Frederico Garcia <fmotagarcia@gmail.com>
2014-02-16 22:50:58 +0000

    implements QuadBatch

---
+++ sparrow-sharp/Sparrow/Core/BaseEffect.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Sparrow.Core
+{
+	public class BaseEffect
+	{
+		public BaseEffect ()
+		{
+		}
+	}
+}
+
--- sparrow-sharp/Sparrow/Display/Quad.cs
+++ sparrow-sharp/Sparrow/Display/Quad.cs
@@ -16,7 +16,7 @@ namespace Sparrow.Display
 				return ColorOfVertex (0);
 			}
 			set {
-				for (int i=0; i<4; ++i) {
+				for (int i = 0; i < 4; ++i) {
 					_vertexData.SetColor (value, i);
 				}
 				
@@ -29,6 +29,7 @@ namespace Sparrow.Display
 				}
 			}
 		}
+
 		public override double Alpha {
 			get {
 				return base.Alpha;
@@ -59,55 +60,52 @@ namespace Sparrow.Display
 			}
 		}
 
-		//		- (SPTexture *)texture
-		//		{
-		//			return nil;
-		//		}
+		public Sparrow.Core.Texture Texture {
+			get { return null; }
+		}
 
 		public Quad (double width = 32, double height = 32, uint color = 0xffffff, bool premultipliedAlpha = false)
 		{
-			if (width  <= MIN_SIZE) width  = MIN_SIZE;
-			if (height <= MIN_SIZE) height = MIN_SIZE;
+			if (width <= MIN_SIZE)
+				width = MIN_SIZE;
+			if (height <= MIN_SIZE)
+				height = MIN_SIZE;
 
 			_tinted = color != 0xffffff;
 
 			_vertexData = new VertexData (4, premultipliedAlpha);
-			_vertexData.Vertices[1].Position.X = width;
-			_vertexData.Vertices[2].Position.Y = height;
-			_vertexData.Vertices[3].Position.X = width;
-			_vertexData.Vertices[3].Position.Y = height;
+			_vertexData.Vertices [1].Position.X = width;
+			_vertexData.Vertices [2].Position.Y = height;
+			_vertexData.Vertices [3].Position.X = width;
+			_vertexData.Vertices [3].Position.Y = height;
 
-			for (int i=0; i<4; ++i) {
+			for (int i = 0; i < 4; ++i) {
 				_vertexData.Vertices [i].Color = new VertexColor (color, 1.0);
 			}
 
 			VertexDataDidChange ();
 		}
 
-
 		public override Rectangle BoundsInSpace (DisplayObject targetSpace)
 		{
 			Point bottomRight;
 
-			if (targetSpace == this) // optimization
-			{
-				bottomRight = _vertexData.PositionAtIndex(3);
-				return new Rectangle(0.0, 0.0, bottomRight.X, bottomRight.Y);
-			}
-			else if (targetSpace == Parent && Rotation == 0.0) // optimization
-			{
+			if (targetSpace == this) { // optimization
+				bottomRight = _vertexData.PositionAtIndex (3);
+				return new Rectangle (0.0, 0.0, bottomRight.X, bottomRight.Y);
+			} else if (targetSpace == Parent && Rotation == 0.0) { // optimization
 				double scaleX = ScaleX;
 				double scaleY = ScaleY;
 
-				bottomRight= _vertexData.PositionAtIndex (3);
+				bottomRight = _vertexData.PositionAtIndex (3);
 				Rectangle resultRect = new Rectangle (X - PivotX * scaleX, 
-				                                      Y - PivotY * scaleY,
-				                                      bottomRight.X * ScaleX,
-				                                      bottomRight.Y * ScaleY); 
+					                       Y - PivotY * scaleY,
+					                       bottomRight.X * ScaleX,
+					                       bottomRight.Y * ScaleY); 
 
 
 				if (scaleX < 0.0) { 
-					resultRect.Width  *= -1.0; 
+					resultRect.Width *= -1.0; 
 					resultRect.X -= resultRect.Width;  
 				}
 
@@ -117,36 +115,36 @@ namespace Sparrow.Display
 				}
 
 				return resultRect;
-			}
-			else
-			{
+			} else {
 				Matrix transformationMatrix = TransformationMatrixToSpace (targetSpace);
 				return _vertexData.BoundsAfterTransformation (transformationMatrix, 0, 4);
 			}
 		}
 
-		public void SetColor(uint color, int vertexID) {
+		public void SetColor (uint color, int vertexID)
+		{
 			_vertexData.SetColor (color, vertexID);
 			VertexDataDidChange ();
 
-				if(color != 0xFFFFFF) {
+			if (color != 0xFFFFFF) {
 				_tinted = true;	
-			}
-			else {
+			} else {
 				_tinted = Alpha != 1.0 || _vertexData.Tinted;
 			}
 		}
 
-		public uint ColorOfVertex(int vertexID)
+		public uint ColorOfVertex (int vertexID)
 		{
 			return _vertexData.ColorAtIndex (vertexID);
 		}
 
-		override public void Render(RenderSupport support) {
+		override public void Render (RenderSupport support)
+		{
 			support.BatchQuad (this);
 		}
 
-		public void SetAlpha(double alpha, int vertexID) {
+		public void SetAlpha (double alpha, int vertexID)
+		{
 			_vertexData.SetAlpha (alpha, vertexID);
 			VertexDataDidChange ();
 
@@ -157,16 +155,18 @@ namespace Sparrow.Display
 			}
 		}
 
-		public double AlphaOfVertex(int vertexID) {
+		public double AlphaOfVertex (int vertexID)
+		{
 			return _vertexData.AlphaAtIndex (vertexID);
 		}
 
-
-		public void VertexDataDidChange() {
+		public void VertexDataDidChange ()
+		{
 
 		}
 
-		public void CopyVertexDataTo(VertexData targetData, int atIndex) {
+		public void CopyVertexDataTo (VertexData targetData, int atIndex)
+		{
 			_vertexData.CopyToVertexData (targetData, atIndex);
 		}
 	}
