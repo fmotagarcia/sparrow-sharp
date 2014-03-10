cd6df6ba0f608ca5cff16087698c0705cceb76b9
Frederico Garcia <fmotagarcia@gmail.com>
2014-03-10 23:11:05 +0000

    embedded benchmark_object

--- SparrowSharp.Samples.Android/Resources/Resource.designer.cs
+++ SparrowSharp.Samples.Android/Resources/Resource.designer.cs
@@ -45,16 +45,19 @@ namespace SparrowSharp.Samples.Android
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int exampleImage = 2130837504;
+			public const int benchmark_object = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int exampleImageJPG = 2130837505;
+			public const int exampleImage = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int Icon = 2130837506;
+			public const int exampleImageJPG = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int monoandroidsplash = 2130837507;
+			public const int Icon = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int monoandroidsplash = 2130837508;
 			
 			static Drawable()
 			{
