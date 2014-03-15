da3c9d0638d821bb6312ec315da0e2a6f770bab1
Frederico Garcia <fmotagarcia@gmail.com>
2014-03-15 14:28:18 +0000

    inlined equals in numberutil

--- SparrowSharp.Core/Utils/NumberUtil.cs
+++ SparrowSharp.Core/Utils/NumberUtil.cs
@@ -6,17 +6,12 @@ namespace Sparrow.Utils
 	{
 		public static bool Equals (float a, float b)
 		{
-			return Equals (a, b, 0.000001f);
+			return Math.Abs (a - b) < 0.000001f;
 		}
 
-		public static bool Equals (float a, float b, float tolerance)
+		public static float Clamp (float value, float min, float max)
 		{
-			return Math.Abs (a - b) < tolerance;
-		}
-
-		public static float Clamp(float value, float min, float max) {
 			return Math.Min (max, Math.Max (value, min));
 		}
 	}
 }
-
