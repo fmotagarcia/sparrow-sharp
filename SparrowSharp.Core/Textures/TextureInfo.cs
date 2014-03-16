a86d8756c02dac9bdf383529378a1aae5423a746
Matyas Forian Szabo <matyas.szabo@plumbeee.co.uk>
2014-03-16 22:30:26 +0000

    add texture atlas

---
+++ SparrowSharp.Core/Textures/TextureInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Sparrow.Geom;
+
+namespace Sparrow.Textures
+{
+	internal class TextureInfo
+	{
+		public readonly Rectangle Region;
+		public readonly Rectangle Frame;
+		public readonly bool Rotated;
+
+		public TextureInfo (Rectangle region, Rectangle frame, bool rotated)
+		{
+			Region = region.Copy ();
+			Frame = frame.Copy ();
+			Rotated = rotated;
+		}
+
+	}
+}
+
